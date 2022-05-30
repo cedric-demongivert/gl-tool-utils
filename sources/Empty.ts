1a5fafab0196f746efc72f385087180293223a30
Cédric <cedric.demongivert@gmail.com>
2022-05-30 11:11:32 +0200

    empty factories

--- sources/Empty.ts
+++ sources/Empty.ts
@@ -8,9 +8,23 @@ export namespace Empty {
   export const STRING: string = ''
 
   /**
+   * 
+   */
+  export function string(): string {
+    return STRING
+  }
+
+  /**
    * An empty numerical value.
    */
   export const NUMBER: number = 0
+
+  /**
+   * 
+   */
+  export function number(): number {
+    return NUMBER
+  }
 
   /**
    * An empty boolean value.
@@ -18,9 +32,23 @@ export namespace Empty {
   export const BOOLEAN: boolean = false
 
   /**
+   * 
+   */
+  export function boolean(): boolean {
+    return BOOLEAN
+  }
+
+  /**
    * An empty array.
    */
   export const ARRAY: readonly any[] = Object.freeze([])
+
+  /**
+   * 
+   */
+  export function array<Element>(): readonly Element[] {
+    return ARRAY
+  }
 
   /**
    * An empty map.
@@ -28,12 +56,33 @@ export namespace Empty {
   export const MAP: Readonly<Map<any, any>> = Object.freeze(new Map())
 
   /**
+   * 
+   */
+  export function map<Key, Value>(): Readonly<Map<Key, Value>> {
+    return MAP
+  }
+
+  /**
    * An empty set.
    */
   export const SET: Readonly<Set<any>> = Object.freeze(new Set())
 
   /**
+   * 
+   */
+  export function set<Value>(): Readonly<Set<Value>> {
+    return SET
+  }
+
+  /**
    * An empty object.
    */
-  export const OBJECT: Readonly<{}> = Object.freeze({})
+  export const OBJECT: Readonly<object> = Object.freeze({})
+
+  /**
+   * 
+   */
+  export function object(): Readonly<object> {
+    return OBJECT
+  }
 }
